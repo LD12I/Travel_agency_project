7f5105a43ef3e7b8798ceda8b278fd8e8727fd13
David Watson <jenokiss187@gmail.com>
2025-05-17 16:43:19 +0200

    Add project files.

---
+++ Travel_agency_project/ServerConnection.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace Travel_agency_project
+{
+    public class ServerConnection
+    {
+        public static HttpClient client = new HttpClient();
+
+
+        public async Task<bool> Registration(string username, string password)
+        {
+            string url = $"http://localhost:3000/register";
+            try
+            {
+                
+                var jsonData = new
+                {
+                    registerUsername = username,
+                    registerPassword = password
+                };
+                
+
+                string json = JsonConvert.SerializeObject(jsonData);
+                using (var request = new HttpRequestMessage(HttpMethod.Post, url))
+                {
+                    request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = await client.SendAsync(request);
+
+                    string responseText = await response.Content.ReadAsStringAsync();
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        System.Windows.MessageBox.Show($"Hiba történt: {responseText}");
+                        return false;
+                    }
+
+                    if (!responseText.StartsWith("{"))
+                    {
+                        return true;
+                    }
+
+                    try
+                    {
+                        JsonResponse responseJson = JsonConvert.DeserializeObject<JsonResponse>(responseText);
+                        System.Windows.MessageBox.Show(responseJson.message);
+                        return true;
+                    }
+                    catch (JsonException)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.Windows.MessageBox.Show($"Hiba: {e.Message}");
+                return false;
+            }
+        }
+
+
+        public async Task<string> LoginAsync(string username, string password)
+        {
+            var requestBody = new
+            {
+                loginUsername = username,
+                loginPassword = password
+            };
+
+            var json = JsonConvert.SerializeObject(requestBody);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("http://localhost:3000/login", content);
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return "Error";
+            }
+
+            var responseData = JsonConvert.DeserializeObject<dynamic>(responseBody);
+            return responseData.token;
+        }
+
+
+        public async Task<string> GetProfileDataAsync(string token)
+        {
+            try
+            {
+                string url = "http://localhost:3000/users/me";
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    return json;
+                }
+                else
+                {
+                    return $"Hiba: {response.StatusCode}";
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Hiba történt: {ex.Message}";
+            }
+        }
+    }
+}
+
+public class JsonResponse
+{
+    public string username { get; set; }
+    public string password { get; set; }
+    public string message { get; set; }
+    public string token { get; set; }
+
+}
---
+++ Travel_agency_project/Travel_agency_idk_mainwindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Travel_agency_project
+{
+    /// <summary>
+    /// Interaction logic for Travel_agency_idk_mainwindow.xaml
+    /// </summary>
+    public partial class Travel_agency_idk_mainwindow : Window
+    {
+        string tokenSaver;
+
+        ServerConnection sc = new ServerConnection();
+
+        public Travel_agency_idk_mainwindow(string token)
+        {
+            InitializeComponent();
+            tokenSaver = token;
+        }
+
+        private async void btnGetProfile_Click(object sender, RoutedEventArgs e)
+        {
+            string adat = await sc.GetProfileDataAsync(tokenSaver);
+
+
+            if (adat.StartsWith("Hiba"))
+            {
+                MessageBox.Show(adat);
+                return;
+            }
+
+            try
+            {
+                var profil = JsonConvert.DeserializeObject<JsonResponse>(adat);
+                if (profil != null)
+                {
+                    MessageBox.Show(profil.username + " , " + profil.password);
+                    nameTB.Text = profil.username;
+                    passwordTB.Text = profil.password;
+                }
+                else
+                {
+                    MessageBox.Show("sikertelen");
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("hiba:" + err.Message);
+            }
+
+
+        }
+    }
+}
